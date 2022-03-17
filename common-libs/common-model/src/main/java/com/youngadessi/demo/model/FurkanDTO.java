d3ea439fa02ac3c1feaf86d6b78e78deab6ed1c7
furkansoyturk <furkansoyturk@icloud.com>
2022-03-17 21:30:42 +0300

    Initial commit

---
+++ common-libs/common-model/src/main/java/com/youngadessi/demo/model/FurkanDTO.java
@@ -0,0 +1,14 @@
+package com.youngadessi.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FurkanDTO {
+
+    private Long id;
+    private String name;
+    private int age;
+
+}
---
+++ common-services/post-service/src/main/java/com/youngadessi/demo/post/PostController.java
@@ -0,0 +1,13 @@
+package com.youngadessi.demo.post;
+
+import com.youngadessi.demo.model.FurkanDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("posts")
+public class PostController {
+
+
+}
