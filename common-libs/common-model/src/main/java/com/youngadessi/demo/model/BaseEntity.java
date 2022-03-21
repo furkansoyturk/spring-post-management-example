b872abca6039ca568161f812e0a08cac9dd1cb4a
Şeyda Nur DEMİR <dmrsydnr@gmail.com>
2022-03-21 23:24:26 +0300

    Moved base entity to common model, Removed sample dto s

---
+++ common-libs/common-model/src/main/java/com/youngadessi/demo/model/BaseEntity.java
@@ -0,0 +1,46 @@
+package com.youngadessi.demo.model;
+
+import lombok.*;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.hibernate.annotations.UpdateTimestamp;
+
+//@Data
+@MappedSuperclass
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@Column(name = "id")
+    private Long id;
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private Long createdBy;
+
+    @CreationTimestamp
+    @Column(name = "created_date_time")
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private Long updatedBy;
+
+    @UpdateTimestamp
+    @Column(name = " updated_date_time")
+    private LocalDateTime updatedDateTime;
+
+    @Version
+    @Column(name = "version")
+    private  Integer version=0;
+
+}
