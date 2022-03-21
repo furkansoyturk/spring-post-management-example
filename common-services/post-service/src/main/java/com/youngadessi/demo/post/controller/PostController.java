9666fc5fbd528607477bdbbc5c208223908af8a5
Şeyda Nur DEMİR <dmrsydnr@gmail.com>
2022-03-21 12:10:29 +0300

    Edited post controller

--- common-services/post-service/src/main/java/com/youngadessi/demo/post/controller/PostController.java
+++ common-services/post-service/src/main/java/com/youngadessi/demo/post/controller/PostController.java
@@ -3,7 +3,9 @@ package com.youngadessi.demo.post.controller;
 //import com.youngadessi.demo.post.model.mapper.PostMapper;
 import lombok.RequiredArgsConstructor;
 //import org.mapstruct.factory.Mappers;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +15,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/post")
 public class PostController {
 
-    //private final PostService postService;
+    /*@Autowired
+    private final PostService postService;*/
     //private static final PostMapper POST_MAPPER = Mappers.getMapper(PostMapper.class);
+
+    @GetMapping
+    public String sampleGet() {
+        return "Return OK";
+    }
+
+    /*@GetMapping(value = "/all")
+    public String getAll() {
+        return "Take All";
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> getSamplePost(@PathVariable Long id) {
+        return new ResponseEntity(HttpStatus.OK);
+    }*/
+
+    @GetMapping(value = "/get")
+    public HttpStatus getSamplePost() {
+        return HttpStatus.OK;
+    }
 
 }
