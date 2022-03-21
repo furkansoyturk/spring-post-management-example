bba2fe0a5a7b9dd7ff9055e5d52195624affdddb
Şeyda Nur DEMİR <dmrsydnr@gmail.com>
2022-03-21 11:58:04 +0300

    Edited user entity

--- common-services/user-service/src/main/java/com/youngadessi/demo/user/model/entity/User.java
+++ common-services/user-service/src/main/java/com/youngadessi/demo/user/model/entity/User.java
@@ -1,27 +1,33 @@
 package com.youngadessi.demo.user.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
 
 @Data
-@Entity
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+@Entity(name = "User")
+//@Table(name = "user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Size(min = 8, max = 32, message = "username length should be between 8 and 32 characters")
+    //@Size(min = 8, max = 32, message = "username length should be between 8 and 32 characters")
     //@Column(name = "username", nullable = false, unique = true)
     private String username;
 
-    @Size(min = 8, max = 32, message = "password length should be between 8 and 32 characters")
+    //@Size(min = 8, max = 32, message = "password length should be between 8 and 32 characters")
     //@Column(name = "password", nullable = false)
     private String password;
 
-    @Version
     //@Column(name = "version")
     private  Integer version;
 
