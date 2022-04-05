60f141ecc8a5af5ca3c2c42530899a1a70012a33
furkansoyturk <furkansoyturk@icloud.com>
2022-04-05 23:23:24 +0300

    spring-cloud-gateway

--- common-services/gateway-service/src/main/java/com/youngadessi/demo/GatewayServiceApplication.java
+++ common-services/gateway-service/src/main/java/com/youngadessi/demo/GatewayServiceApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class GatewayServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
 
 }
