67c41f5a1af763a533fcbd4bab0c3a788a9fc74a
furkansoyturk <furkansoyturk@icloud.com>
2022-04-06 03:10:13 +0300

    gateway & aut-service (still not working)

--- common-services/gateway-service/src/main/java/com/youngadessi/demo/GatewayServiceApplication.java
+++ common-services/gateway-service/src/main/java/com/youngadessi/demo/GatewayServiceApplication.java
@@ -2,9 +2,10 @@ package com.youngadessi.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @SpringBootApplication
-
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
--- common-services/gateway-service/src/main/java/com/youngadessi/demo/RouterValidator.java
+++ common-services/gateway-service/src/main/java/com/youngadessi/demo/RouterValidator.java
@@ -3,22 +3,23 @@ package com.youngadessi.demo;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.function.Predicate;
-
-@Component
-public class RouterValidator {
-
-    public static final List<String> openApiEndpoints= List.of(
-            "/auth/register",
-            "/auth-service/login"
-    );
-
-    public Predicate<ServerHttpRequest> isSecured =
-            request -> openApiEndpoints
-                    .stream()
-                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
-}
+//import java.util.List;
+//import java.util.function.Predicate;
+//
+//@Component
+//public class RouterValidator {
+//
+//    public static final List<String> openApiEndpoints= List.of(
+//            "/auth/register",
+//            "/auth-service/login",
+//            "/post-service/**"
+//    );
+//
+//    public Predicate<ServerHttpRequest> isSecured =
+//            request -> openApiEndpoints
+//                    .stream()
+//                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+//}
 
 // http.csrf().disable()
 //         .authorizeRequests().antMatchers("/login").permitAll()
